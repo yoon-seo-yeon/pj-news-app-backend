8da79ed7c1bfb85f74b512c4a6ad980cc1e97a1d
Seoyeon Yoon <s2013@e-mirim.hs.kr>
2022-05-31 13:32:38 +0900

    app.js 코드 수정

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// build 폴더를 static 폴더로 사용하도록 수정
+// (build 폴더의 경로는 이름 바꾸거나 수정이 가능함)
+app.use(express.static(path.join(__dirname, '/build')));
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// 그 외 요청은 모두 리액트에서 빌드한 폴더의 index.html 보내기
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+// express-generator로 만든 경우 모듈로 내보내야 함
 module.exports = app;
